e00cc6bac66c9be65ba8a5c566212626ad386f7f
JeeveshN <narang.jeevesh@gmail.com>
2016-10-27 11:07:49 +0530

    PipoAdder added

--- combinational/arithmetics.py
+++ combinational/arithmetics.py
@@ -34,3 +34,27 @@ class FullAdder(new.Hardware,object):
         self.components.append(HalfAdder([x[0],x[1]],[self.internalWiring[0], self.internalWiring[1]]))
         self.components.append(HalfAdder([self.internalWiring[1],x[2]],[self.internalWiring[2],s[1]]))
         self.components.append(gates.OrGate([self.internalWiring[0]],[self.internalWiring[2]],[s[0]]))
+
+class PipoAdder(new.Hardware,object):
+
+    def __init__(self,x,y,s):
+        try:
+            (len(x) != len(y) or len(s) != len(x)+1)
+        except NotImplementedError:
+            print('Invalid Connections')
+        super(PipoAdder,self).__init__([x,y,s])
+        input = []
+        input.append(x)
+        input.append(y)
+        self.input=input
+        self.output=s
+        size = len(x)
+        self.internalWiring = transport.wires(size)
+        if size > 1:
+            self.components.append(FullAdder([x[0], y[0], self.internalWiring[0]], [self.internalWiring[1], s[size]]))
+            for i in xrange(size-1):
+                self.components.append(FullAdder([x[i], y[i], self.internalWiring[i]], [self.internalWiring[i+1], s[size-i]]))
+            self.components.append(FullAdder([x[size-1], y[size-1], self.internalWiring[size-1]], [s[0], s[1]]))
+        else:
+            self.components.append(FullAdder([x[0], y[0], self.internalWiring[0]], [s[0], s[1]]))
+        self.internalWiring[0].propagateSignal(0)
